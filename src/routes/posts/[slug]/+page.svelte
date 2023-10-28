51df077dc23e293de0da6a0e1205cfa9b265e353
Kevin Devlin <azwethinkweizkd@gmail.com>
2023-10-27 19:04:24 -0600

    learned about layout and server files

---
+++ src/routes/posts/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type {PageData} from "./$types";
+
+    export let data: PageData;
+
+    function formatDate(date: Date) {
+        return new Intl.DateTimeFormat('en', {
+            dateStyle: 'long'
+        }).format(date)
+    };
+
+    $: ({post} = data)
+</script>
+
+<hgroup>
+    <h1>{post?.title}</h1>
+    <h2>{formatDate(post?.createdAt)}</h2>
+</hgroup>
+
+
+<div class="content">
+    {@html post?.content}
+</div>
